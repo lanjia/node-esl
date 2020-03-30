a22e1c7619ec10923ed22fdc2a58a3d8f2bc8ecb
LANJIA <rails@qq.com>
2020-03-30 22:53:08 +0800

    fix

--- test/inbound.js
+++ test/inbound.js
@@ -1,28 +1,48 @@
-const esl = require('../lib/index')
+const ESL = require('../lib/index')
 
-const conn = esl.Connection.createInbound('127.0.0.1', 8021, '56766566', async () => {
-  conn.on('esl::event::CHANNEL_CREATE', (evt) => {
-    console.debug('-----------CHANNEL_CREATE-----------------')
-    console.log('CHANNEL_CREATE:', evt.serialize())
-  })
+const conn = ESL.Connection.createInbound('127.0.0.1', 8021, '56766566', async () => {
+  // conn.on('esl::event::CHANNEL_CREATE', (evt) => {
+  //   console.debug('-----------CHANNEL_CREATE-----------------')
+  //   console.log('CHANNEL_CREATE:', evt.serialize())
+  // })
 
-  conn.on('esl::event::CHANNEL_DESTROY', () => {
-    console.debug('-----------CHANNEL_DESTROY-----------------')
-  })
+  // conn.on('esl::event::CHANNEL_DESTROY', () => {
+  //   console.debug('-----------CHANNEL_DESTROY-----------------')
+  // })
 
-  conn.on('esl::event::CHANNEL_CALLSTATE', () => {
-    console.debug('-----------CHANNEL_CALLSTATE-----------------')
-  })
+  // conn.on('esl::event::CHANNEL_CALLSTATE', () => {
+  //   console.debug('-----------CHANNEL_CALLSTATE-----------------')
+  // })
+
+  // let err
+  // let res
+  // [err, res] = await conn.subscribe(['BACKGROUND_JOB', 'CHANNEL_CREATE'])
+  // console.log('subscribe res:', res.getHeader('success'))
+  // // let err2
+  // [err, res] = await conn.api('json {"command" : "status", "data" : ""}', 2000)
+  // if (!err) {
+  //   console.log('api res:', res)
+  // }
+  console.log('-------begin---------')
 
   let res = await conn.subscribe(['BACKGROUND_JOB', 'CHANNEL_CREATE'])
-  console.log('subscribe res:', res.getHeader('success'))
+  if (!res.err) {
+    console.log('subscribe res:', res.data.getHeader('success'))
+  }
 
-  // res = await conn.api('json {"command" : "status", "data" : ""}')
-  // console.log('api res:', res.getBody())
+  res = await conn.api('json {"command" : "status", "data" : ""}', 2000)
+  if (!res.err) {
+    console.log('api res:', res.data)
+  }
 
-  res = await conn.api('show registrations')
-  console.log('show registrations res:', res.getBody())
+  console.log('-------end---------')
 
-  // res = await conn.api('show registrations as json')
-  // console.log('show registrations as json res:', res.getBody())
+  // res = await conn.api('show registrations')
+  // console.log(`[${res}]`)
+
+  // res = await conn.api('show1 registrations as json')
+  // console.log(`[${res}]`)
+
+  // res = await conn.api('callcenter_config1 agent set status 10111 Available')
+  // console.log(`[${res}]`)
 })
--- test/outbound.js
+++ test/outbound.js
@@ -1,25 +1,44 @@
-const esl = require('../lib/index')
+const ESL = require('../lib/index')
 
-const server = new esl.Server('127.0.0.1', 9001, () => {
+const server = new ESL.Server('127.0.0.1', 9001, () => {
   console.log('esl server is up')
 })
 
 server.on('connection::ready', async (conn) => {
   console.log('connection::ready')
   // console.log(conn.getInfo())
-  conn.on('esl::event::CHANNEL_ANSWER', async () => {
+  conn.on('esl::event::CHANNEL_ANSWER', async (evt) => {
     console.log('CHANNEL_ANSWER')
+
+    console.log('playback start')
     console.time('playback')
-    await conn.execute('playback', 'voc/welcome.wav', 20000)
+    let res = await conn.execute('playback', 'voc/welcome.wav', 20000)
+    if (res.err) {
+      console.log('playback err:', res.err.message)
+    } else {
+      console.log('playback res:', res.data.serialize())
+    }
     console.timeEnd('playback')
+
+    console.log('bridge start')
+    console.time('bridge')
+    res = await conn.execute('bridge', 'user/1011', 200000)
+    if (res.err) {
+      console.log('bridge err:', res.err.message)
+    } else {
+      console.log('bridge res:', res.data.serialize())
+    }
+    console.timeEnd('bridge')
   })
 
   conn.on('esl::end', async () => {
     console.debug('esl::end')
   })
 
-  await conn.execute('answer')
-  // console.log('answer:', res.serialize())
+  const res = await conn.execute('answer')
+  if (res.err) {
+    console.log('answer err:', res.err.message)
+  }
 })
 
 // server.on('connection::close', async () => {
